53981ccce8bb6c7a1df4f07eb2d170344e855062
Harshith Gowda <harshithgowdakt@gmail.com>
2023-05-16 19:53:55 +0530

    added logs for methods

--- oops/interface.go
+++ oops/interface.go
@@ -1,5 +1,7 @@
 package oops
 
+import "fmt"
+
 type store interface {
 	read(duid string)
 	write(data string, uid string)
@@ -16,18 +18,22 @@ func NewFileStore() *filestore {
 
 func (f *filestore) write(data string, uid string) {
 	// will write implementation later
+	fmt.Println("Writing data to file")
 }
 
 func (f *filestore) update(data string, uid string) {
 	// will write implementation later
+	fmt.Println("Updating data in the file")
 }
 
 func (f *filestore) read(uid string) {
 	// will write implementation later
+	fmt.Println("reading data from the file")
 }
 
 func (f *filestore) delete(uid string) {
 	// will write implementation later
+	fmt.Println("deleting data from the file")
 }
 
 type s3store struct {
@@ -39,18 +45,22 @@ func NewS3Store() *s3store {
 
 func (s *s3store) write(data string, uid string) {
 	// will write implementation later
+	fmt.Println("Writing data to s3")
 }
 
 func (s *s3store) update(data string, uid string) {
 	// will write implementation later
+	fmt.Println("updating data in the s3")
 }
 
 func (s *s3store) read(uid string) {
 	// will write implementation later
+	fmt.Println("reading data from s3")
 }
 
 func (s *s3store) delete(uid string) {
 	// will write implementation later
+	fmt.Println("deleting data from s3")
 }
 
 func GetStorage(storeType string) store {
@@ -64,15 +74,14 @@ func GetStorage(storeType string) store {
 	}
 }
 
-func runStoreExample(){
-	s:=GetStorage("file");
+func RunStoreExample() {
+	s := GetStorage("file")
 	s.write("hello, world", "123")
 	s.read("123")
 	s.update("this is updated data", "123")
 	s.delete("123")
 
-
-	s= GetStorage("s3")
+	s = GetStorage("s3")
 	s.write("write this to s3", "123")
 	s.read("123")
 	s.update("updated data", "123")
