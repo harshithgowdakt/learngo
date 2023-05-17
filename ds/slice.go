b794326cb02258a2825011710e34b90bae0947a6
Harshith Gowda <harshithgowdakt@gmail.com>
2023-05-17 23:29:35 +0530

    added slice examples

--- ds/slice.go
+++ ds/slice.go
@@ -2,19 +2,44 @@ package ds
 
 import "fmt"
 
+func RunSliceExmaples() {
+	numbers := []int{1, 2, 3, 4, 5, 6}
+	slice := numbers[1:4]
 
-func RunSliceExmaples(){
-	numbers := []int{1,2,3,4,5,6}
-	slice :=numbers[1:4]
-
-	fmt.Println("slice :: ",slice)
+	fmt.Println("slice :: ", slice)
 	fmt.Println("slice len ::", len(slice))
 	fmt.Println("slice cap ::", cap(slice))
 
 	slice2 := numbers[:3]
 	fmt.Println("slice2 ::", slice2)
 
-	slice3 :=numbers[3:]
+	slice3 := numbers[3:]
 
 	fmt.Println("slice3 ::", slice3)
+
+	// concat example
+	s1 := []int{1, 2, 3}
+	s2 := []int{4, 5, 6}
+	s3 := append(s1, s2...)
+	fmt.Println("concatinated slice ::", s3)
+
+	numbers = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var filtered []int
+	var found int
+	for _, num := range numbers {
+		if num%2 == 0 {
+			filtered = append(filtered, num)
+		}
+	}
+
+	for _, num := range numbers {
+		if num == 3 {
+			found = num
+			break
+		}
+
+	}
+
+	fmt.Println("found number ::", found)
+	fmt.Println("filtered array ::", filtered)
 }
